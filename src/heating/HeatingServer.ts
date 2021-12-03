cd7938e24150e1ca2e461344b272e0ae72b70ceb
jas777 <jas.czeszejko@wp.pl>
2021-12-03 11:30:39 +0100

    Added inloop endpoint

--- src/heating/HeatingServer.ts
+++ src/heating/HeatingServer.ts
@@ -69,6 +69,10 @@ export const startServer = async (config: Configuration, heaters: Heater[]) => {
         res.code(200).send(JSON.stringify(config));
     });
 
+    app.get('inloop', (_req, res) => {
+        res.code(200).send({ in_loop: heatersAuto });
+    })
+
     app.listen(2137, '0.0.0.0', (err: any, addr: string) => {
         if (err) {
             console.error(err);
