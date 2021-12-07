255c592672ce2580b20788323c2d9fbbbd3f3010
jas777 <jas.czeszejko@wp.pl>
2021-12-07 12:37:36 +0100

    wejuitwe

--- src/heating/HeatingServer.ts
+++ src/heating/HeatingServer.ts
@@ -105,8 +105,8 @@ export const startServer = async (config: Configuration, heaters: Heater[]) => {
 
             fs.writeFileSync(Path.resolve('ogrzewanie.config.json'), JSON.stringify(config, null, 4));
 
-            // clearInterval(<NodeJS.Timeout>loop);
-            // startLoop(config.interval, config.duration);
+            clearInterval(<NodeJS.Timeout>loop);
+            startLoop(config.interval, config.duration);
 
             res.code(200).send('OK');
         } else {
